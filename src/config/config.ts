38c7e9976b8cd9fa20259db03c8da658d047234d
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-06-20 20:42:31 +0100

    exercise files
    Went through the udacity-c2-restapi repo cloned from udacity and made some changes to be able run on my local machine and deploy.

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramtuyodev",
+    "password": "Adedolapo1$",
+    "database": "udagramtuyodev",
+    "host": "udagramtuyodev.cbj9ilbr84kc.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagramtuyodevbucket"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
